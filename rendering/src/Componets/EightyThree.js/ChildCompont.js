a198a431a3264be3da9290ffed371786a6b87f52
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-08-23 15:32:55 +0530

    lerning about usestate,and usememo rendring

--- rendering/src/Componets/EightyThree.js/ChildCompont.js
+++ rendering/src/Componets/EightyThree.js/ChildCompont.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const ChildCompont = () => {
 console.log("Child componet Rendering")
+const date=new Date()
   return (
     <div>
       ChildCompont
+      {date.getMonth()}
+      {date.getHours()}
     </div>
   )
 }
