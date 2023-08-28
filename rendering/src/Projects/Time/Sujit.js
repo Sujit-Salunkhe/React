c8d60195b8995207a5ed73a3492e1c7bb62ad05b
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-08-28 14:47:40 +0530

    adding color to greeting

--- rendering/src/Projects/Time/Sujit.js
+++ rendering/src/Projects/Time/Sujit.js
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const Sujit = () => {
-  let currenttime = new Date(2020, 2, 2, 7);
+  let currenttime = new Date();
   let currethours = currenttime.getHours();
   console.log(currethours);
   const [Greeting, setGreeting] = useState("Good Night");
+  const [grettingStyle,setStyle]=useState({color:"black"})
   useEffect(() => {
     if (currethours >= 6 && currethours < 12) {
       setGreeting("Good Morning");
-      console.log("greeting");
+      console.log("good morning")
+      setStyle({color:"green"})
+      console.log(grettingStyle)
+      // console.log("greeting");
     } else if (currethours >= 12 && currethours < 18) {
-      setGreeting("Good Afternoon");
+      // grettingStyle.color="pink"
+
+      setGreeting("Good Afternoon")
+      setStyle({color:"pink"});
+      console.log("good afternoon")
+      console.log(grettingStyle)
     }
   }, [currethours]);
 
+
   return (
     <div>
-      <h1>Hello sir ,{Greeting}</h1>
+      <h1>Hello sir ,<span style={grettingStyle}>{Greeting}</span></h1>
     </div>
   );
 };
