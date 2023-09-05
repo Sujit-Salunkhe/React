09b3fc89cf77ef96a6018710fd5ddc8b73815bd7
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-05 14:54:08 +0530

    creating store in redux first time

--- Redux/index.js
+++ Redux/index.js
@@ -1,19 +1,30 @@
-const Buy_cake="Buy_cake"
-function Buy_cake () {
-    return {
-        type:Buy_cake,
-        info:"First Redux action"
-    }
-}
-const intialstate ={
-    noOfCakes :10
-}
-const reducer = (state=intialstate,action) =>{
-    switch(action.type){
-        case Buy_cake:return {
-        ...state,
-        noOfCakes:state.noOfCakes - 1
+    const redux=require('redux');
+    const createStore = redux.createStore
+    const BUY_CAKE="BUY_CAKE"
+    function Buy_cake () {
+        return {
+            type:BUY_CAKE,
+            info:"First Redux action"
         }
-        default: return state
     }
-}
+    const initialState = {
+        noOfCakes :10
+    }
+    const reducer = (state=initialState,action) =>{
+        switch(action.type){
+            case BUY_CAKE:
+                return {
+            ...state,
+            noOfCakes:state.noOfCakes - 1
+            }
+            default: return state
+        }
+    }
+    const store = createStore(reducer);
+    console.log('initialState',store.getState())
+    const unsubscribe = store.subscribe(()=>console.log("updated State",store.getState()))
+    store.dispatch(Buy_cake())
+    store.dispatch(Buy_cake())
+    store.dispatch(Buy_cake())
+    store.dispatch(Buy_cake())
+    unsubscribe()
