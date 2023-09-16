fac41490594e6b8ff8c093de3066181f05b4c599
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-16 15:54:06 +0530

    adding dispatch function

--- react-redux/src/componets/ItemContainer.js
+++ react-redux/src/componets/ItemContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buy_IceCreme } from '../redux'
 function ItemContainer(props) {
   return (
     <div>
        <h2>Item -{props.item}</h2>
+       <button onClick={props.buyitem}>buy cake</button>
     </div>
   )
 }
@@ -13,4 +15,10 @@ return {
     item:itemState
 }
 }
-export default connect (mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch,ownProps) =>{
+    const disptachfunc = ownProps.cake ? () => dispatch(buyCake()):() =>  dispatch(buy_IceCreme())
+    return {
+        buyitem:disptachfunc
+    }
+}
+export default connect (mapStateToProps,mapDispatchToProps)(ItemContainer)
