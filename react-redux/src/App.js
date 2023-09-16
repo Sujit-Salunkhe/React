05e17d6a132308bc490d14e33e90eaf686d33894
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-16 15:34:13 +0530

    mapstatetoprops adding second parameter

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -5,10 +5,13 @@ import CakeContainer from './componets/CakeContainer';
 import HooksCakeContainer from './componets/HooksCakeContainer';
 import IceCremeContainer from './componets/IceCremeContainer';
 import NewCakeContainer from './componets/NewCakeContainer';
+import ItemContainer from './componets/ItemContainer';
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+    <ItemContainer cake/>
+    <ItemContainer/>
     <CakeContainer/>
     <NewCakeContainer/>
     <HooksCakeContainer/>
