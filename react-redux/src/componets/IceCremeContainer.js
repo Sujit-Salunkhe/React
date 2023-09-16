0fcece282485df0a95e41de635894cdafac3818b
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2023-09-16 12:44:02 +0530

    adding devtools in chrome and see it's usages

--- react-redux/src/componets/IceCremeContainer.js
+++ react-redux/src/componets/IceCremeContainer.js
@@ -6,13 +6,13 @@ function IceCremeContainer(props) {
   return (
     <div>
       <h2>Number of iceCremes - {props.numOfIceCrems}</h2>
-      <button onClick={props.buy_IceCremes}>Buy IceCreme</button>
+      <button onClick={props.buy_IceCreme}>Buy IceCreme</button>
     </div>
   )
 }
 const mapStateToProps = state =>{
     return {
-        numOfIceCrems:state.iceCreme.noOfIceCrems
+        numOfIceCrems:state.iceCreme.numOfIceCrems
     }
 }
 const mapDispatchToProps = dispatch => {
