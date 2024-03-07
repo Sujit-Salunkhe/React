684301dfa2bedb2b65c32a38935adedfbb64d8db
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-03-07 12:21:36 +0530

    adding input box on button

--- file-explorer/src/componets/Folder.js
+++ file-explorer/src/componets/Folder.js
@@ -1,30 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Folder = ({explorerData}) => {
-
-
-  if (explorerData.isFolder){
+const Folder = ({ explorerData }) => {
+  const [expand, setExpand] = useState(false);
+  const [show, setShow] = useState({
+    visible: false,
+    isFolder: false,
+  });
+  const handleDisplay = (e) => {
+    // setShow({ ...show, visible: false });
+    setExpand(!expand);
+  };
+  const insertFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShow({ visible: true, isFolder });
+  };
+  if (explorerData.isFolder) {
     return (
-      <div className='main-container'>
-        <div className='container'>
-        <span className='foldercontainer'>📁 {explorerData.name}</span>
-        <div>
-          <button className='buttons'>Folder +</button>
-          <button className='buttons'>File +</button>
-          </div>  
+      <div className="main-container">
+        <div className="container" onClick={(e) => handleDisplay(e)}>
+          <span className="foldercontainer">📁 {explorerData.name}</span>
+          <div>
+            <button className="buttons" onClick={(e) => insertFolder(e, true)}>
+              Folder +
+            </button>
+            <button className="buttons" onClick={(e) => insertFolder(e, false)}>
+              File +
+            </button>
           </div>
-    <div style={{paddingLeft:"30px"}}>
-    {explorerData.items.map((exp) => {
-        return (
-          <Folder key={exp.id} explorerData={exp}/>  
-        );
-        })}
+        </div>
+        <div
+          style={{ paddingLeft: "30px", display: expand ? "block" : "none" }}
+        >
+          {show.visible && (
+            <div>
+              <span>{show.isFolder ? "📁" : "📄"}</span>
+              <input type="text" autoFocus className="inputbox" onBlur={()  => setShow({ ...show, visible: false })}></input>
+            </div>
+          )}
+          {explorerData.items.map((exp) => {
+            return <Folder key={exp.id} explorerData={exp} />;
+          })}
+        </div>
       </div>
-      </div>
-    )}else{
-      return <span className='file'>📄 {explorerData.name}</span>
-    }
+    );
+  } else {
+    return <span className="file">📄 {explorerData.name}</span>;
+  }
+};
 
-}
-
-export default Folder
+export default Folder;
