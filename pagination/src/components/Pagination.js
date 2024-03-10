ce5b5cea0390531d8dc4ff788fa5e2bb98f700af
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-03-10 21:13:39 +0530

    fetching data succesfully

--- pagination/src/components/Pagination.js
+++ pagination/src/components/Pagination.js
@@ -1,19 +1,31 @@
-import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Pagination = () => {
-    const [products,setProducts] = useState([])
-    useEffect(() => {
-        axios.get('https://dummyjson.com/products')
-        .then(res => setProducts(res))
-        .catch(err => console.log(err))
-    },[])
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products?limit=100")
+      .then((res) => {
+        setProducts(res.data.products)
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
-      
+      {loading
+        ? "loading..."
+        : products.map((product) => (
+            <div key={product.id}>{product.title}</div>
+          ))}{" "}
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
