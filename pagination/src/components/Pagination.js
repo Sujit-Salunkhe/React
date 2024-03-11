d342fca7cc4b0890098ddc3aaa88aae45907c4c7
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-03-11 18:46:24 +0530

    adding pagination numbers

--- pagination/src/components/Pagination.js
+++ pagination/src/components/Pagination.js
@@ -9,7 +9,7 @@ const Pagination = () => {
     axios
       .get("https://dummyjson.com/products?limit=100")
       .then((res) => {
-        setProducts(res.data.products)
+        setProducts(res.data.products);
         setLoading(false);
       })
       .catch((err) => {
@@ -19,11 +19,36 @@ const Pagination = () => {
   }, []);
   return (
     <div>
-      {loading
-        ? "loading..."
-        : products.map((product) => (
-            <div key={product.id}>{product.title}</div>
-          ))}{" "}
+        <p className="title_head">product images</p>
+      {loading ? (
+        "loading..."
+      ) : (
+        <>
+          <div className="main__container">
+            {products.slice(0, 10).map((product) => (
+              <span key={product.id} className="products__container">
+                <img
+                  src={product.thumbnail}
+                  alt="productImg"
+                  className="images"
+                />
+                <span>{product.title}</span>
+              </span>
+            ))}
+          </div>
+          {products.length > 0 && (
+            <div className="pagination_container">
+              <span> Prev...</span>
+              <span>
+                {[...Array(products.length / 10)].map((_, i) => (
+                  <span key={i + 1} className="pagination_numbers">{i + 1}</span>
+                ))}
+              </span>
+              <span>Next...</span>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
