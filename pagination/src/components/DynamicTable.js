5b4e96df147736b839c3f479d5bb69a7a0367949
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-03-16 16:30:00 +0530

    adding css file

--- pagination/src/components/DynamicTable.js
+++ pagination/src/components/DynamicTable.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import './dy.css'
 const DynamicTable = () => {
-  const [Row, setRow] = useState();
+  const [row, setRow] = useState();
   const [col, setCol] = useState();
+  const [arr,setArr] = useState([1,2,3,4,5,6])
+  const [order,setOrder] = useState()
 
   return (
     <>
@@ -11,9 +13,12 @@ const DynamicTable = () => {
         <input type="text" onChange={(e) => setCol(e.target.value)} placeholder="Col" className="input_con"/>
       </div>
       <div className="radio_container">
-        Order :   <input type="radio" id="radio" className="radio_input" name="order"/>Ascending
-        <input type ="radio" id='radio' className="radio_input" name="order"/>Descending
-      </div>
+        Order :   <input type="radio" id="radio1" className="radio_input" name="order" value={() => setOrder('asc')}/><label for="radio1">Ascending</label>
+        <input type ="radio" id='radio2' className="radio_input" name="order" value={() => setOrder('des')}/><label for='radio2'>Descending</label>
+      </div>  
+     <table>
+        
+     </table>
     </>
   );
 };
