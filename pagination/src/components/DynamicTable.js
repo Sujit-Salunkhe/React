1c22bf808741850c1a2439cc07b299cbffd3420e
Sujit Salunkhe <sujitsalunkhe3738@gmail.com>
2024-03-17 14:07:31 +0530

    solving interview question

--- pagination/src/components/DynamicTable.js
+++ pagination/src/components/DynamicTable.js
@@ -1,24 +1,69 @@
-import React, { useState } from "react";
-import './dy.css'
+import React, { useEffect, useState } from "react";
+import "./dy.css";
 const DynamicTable = () => {
   const [row, setRow] = useState();
   const [col, setCol] = useState();
-  const [arr,setArr] = useState([1,2,3,4,5,6])
-  const [order,setOrder] = useState()
+  const [numbers, setNumbers] = useState(1);
 
+  let number = 1
+  const handelNumber = () => {
+    //   console.log(num,'inside func')
+        number +=1
+  }
   return (
     <>
       <div className="input_Container">
-        <input type="text" onChange={(e) => setRow(e.target.value)} placeholder="Row" />
-        <input type="text" onChange={(e) => setCol(e.target.value)} placeholder="Col" className="input_con"/>
+        <input
+          type="text"
+          onChange={(e) => setRow(e.target.value)}
+          placeholder="Row"
+        />
+        <input
+          type="text"
+          onChange={(e) =>setCol(e.target.value)}
+          placeholder="Col"
+          className="input_con"
+        />
       </div>
       <div className="radio_container">
-        Order :   <input type="radio" id="radio1" className="radio_input" name="order" value={() => setOrder('asc')}/><label for="radio1">Ascending</label>
-        <input type ="radio" id='radio2' className="radio_input" name="order" value={() => setOrder('des')}/><label for='radio2'>Descending</label>
-      </div>  
-     <table>
-        
-     </table>
+        Order :
+        <input
+          type="radio"
+          id="radio1"
+          className="radio_input"
+          name="order"
+          value="asc"
+        />
+        <label htmlFor="radio1">Ascending</label>
+        <input
+          type="radio"
+          id="radio2"
+          className="radio_input"
+          name="order"
+          value="dsc"
+        />
+        <label htmlFor="radio2">Descending</label>
+      </div>
+      <table>
+      {row && col ? (
+        <>
+        <tbody>
+            {[...Array(parseInt(row))].map(( _, i) => (
+                <tr key={i}>
+                 {[...Array(parseInt(row) * parseInt(col))].slice(0,col).map(( _, index) => (
+                    <td key={index}>
+                        {number}
+                        {handelNumber()}
+                    </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          
+          
+          </>
+      ) : null}
+      </table>
     </>
   );
 };
